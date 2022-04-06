594e78a3a4e1ed310eebe0fb73151f975943a027
antoniog675 <antoniogomez1@hotmail.co.uk>
2022-04-06 09:26:59 +0000

    Adjust validation to check that all values provided are integers

--- run.py
+++ run.py
@@ -1,5 +1,4 @@
 import gspread
-
 from google.oauth2.service_account import Credentials
 
 SCOPE = [
@@ -33,6 +32,7 @@ def validate_data(values):
     or if there aren't exactly 6 values
     """
     try:
+        [int(value) for value in values]
         if len(values) != 6:
             raise ValueError(
                 f"Exactly 6 values required, you entered {len(values)}"
